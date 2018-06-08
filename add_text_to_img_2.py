0f4dac0ed53c00305bd00aad2d03bf9371885676
savad <sava.dan42@gmail.com>
2018-06-08 19:15:11 +0300

    Adding support for loading facts from the "DB" and creating the image. NOTE test out if a gif is better might look CRAZY

--- add_text_to_img_2.py
+++ add_text_to_img_2.py
@@ -1,32 +1,26 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-from util import construct_image, add_logo_to_image
+from util import construct_image_frame, add_logo_to_image
 import textwrap
 
-thumb_paths = ["img/%d.jpg" % (x+1) for x in range(57)]
-text = "For the first time in human history, gene-editing has been performed to fix a mutation for an inherited disease in embryos. Using a powerful tool called Crispr-Cas9, scientists successfully altered the DNA in defective embryos so they were no longer programmed to develop congenital heart failure."
-text_wrapped = textwrap.wrap(text, width=40)
-print(len(text_wrapped))
-thumb_size = 50
-img_width = 1200
-img_height = 800
 
-img = construct_image(width=img_width, height=img_height, thumbs_path_list=thumb_paths, thumb_size=thumb_size, clip_rect=(150, 150, 1000, 200 + (thumb_size * len(text_wrapped))))
-draw = ImageDraw.Draw(img)
-width, height = img.size
-font_size = 38
-font_path = "fonts/ChalkFont-Regular.ttf"
-font = ImageFont.truetype(font_path, font_size)
-for line_nr, text_line in enumerate(text_wrapped):
-    print(len(text_line), text_line)
-    draw.text((200+10, 200 + line_nr * (font_size + 10)),
-              text_line,
-              (0, 0, 0),
-              font=font)
+def build_image(img_width, img_height, text, text_wrap_width, font_path,font_size, thumb_size, index_image):
+    thumb_paths = ["img/%d.jpg" % (x+1) for x in range(57)]
+    text_wrapped = textwrap.wrap(text, width=text_wrap_width)
+    img = construct_image_frame(width=img_width, height=img_height, thumbs_path_list=thumb_paths, thumb_size=thumb_size, clip_rect=(150, 150, 1000, 200 + (thumb_size * len(text_wrapped))))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_path, font_size)
 
-# Adding the little bits log to the image
-img = add_logo_to_image(img, "logo/logo.png", thumb_size)
+    for line_nr, text_line in enumerate(text_wrapped):
+        print(len(text_line), text_line)
+        draw.text((200+10, 200 + line_nr * (font_size + 10)),
+                  text_line,
+                  (0, 0, 0),
+                  font=font)
 
-# Saving the image to disk
-img.save('output/little_bits_sci1.jpg')
+    # Adding the little bits log to the image
+    img = add_logo_to_image(img, "logo/logo.png", thumb_size)
+
+    # Saving the image to disk
+    img.save('output/lbs_%d.jpg' % index_image)
--- util.py
+++ util.py
@@ -1,8 +1,11 @@
 from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import textwrap
 from random import shuffle
 
 
-def construct_image(width, height, thumbs_path_list, thumb_size, clip_rect=(150, 150, 1000, 200)):
+def construct_image_frame(width, height, thumbs_path_list, thumb_size, clip_rect=(150, 150, 1000, 200)):
     result = Image.new("RGB", (width, height), color='white')
     nr_lines = int(height/thumb_size)
     nr_columns = int(width/thumb_size)
@@ -33,3 +36,24 @@ def add_logo_to_image(img, logo_path,logo_size):
     logo.thumbnail((logo_size, logo_size), Image.ANTIALIAS)
     img.paste(logo, (img_width - logo_size, img_height - logo_size, img_width, img_height))
     return img
+
+
+def build_image(img_width, img_height, text, text_wrap_width, font_path, font_size, thumb_size, index_image):
+    thumb_paths = ["img/%d.jpg" % (x + 1) for x in range(57)]
+    text_wrapped = textwrap.wrap(text, width=text_wrap_width)
+    img = construct_image_frame(width=img_width, height=img_height, thumbs_path_list=thumb_paths, thumb_size=thumb_size,
+                                clip_rect=(150, 150, 1000, 200 + (thumb_size * len(text_wrapped))))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_path, font_size)
+
+    for line_nr, text_line in enumerate(text_wrapped):
+        draw.text((200 + 10, 200 + line_nr * (font_size + 10)),
+                  text_line,
+                  (0, 0, 0),
+                  font=font)
+
+    # Adding the little bits log to the image
+    img = add_logo_to_image(img, "logo/logo.png", thumb_size)
+
+    # Saving the image to disk
+    img.save('output/lbs_%d.jpg' % index_image)
